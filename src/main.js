9b52ed025ebb287d165be2c9533eadb2c204ffb9
Onazarit <onazarit@ucol.mx>
2020-12-10 15:52:39 -0600

    Implemented function convertirAString

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ export default class App {
         return(fact);
     }
 
+    convertirAString(numero){
+        let string="";
+        while(numero >=1){
+            string = string + "*";
+            numero--;
+        }
+        return(string);
+    }
 }
 
 let app = new App();
@@ -14,3 +22,6 @@ let app = new App();
 console.log("Factorial de 5 = "+app.factorial(5));
 console.log("Factorial de 6 = "+app.factorial(6));
 
+// Prueba metodo convertirAString
+console.log("5 asteriscos: "+app.convertirAString(5));
+console.log("8 asteriscos: "+app.convertirAString(8));
