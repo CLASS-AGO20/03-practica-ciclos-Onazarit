17f1f6ec25742eeca229407554a27154298d5e93
Onazarit <onazarit@ucol.mx>
2020-12-10 15:48:37 -0600

    Implemented function factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-export default class App {}
+export default class App {
+    factorial(numero){
+        let fact=1;
+        for(let i = numero; i>=1;i--){
+            fact = fact * i;
+        }
+        return(fact);
+    }
+
+}
 
 let app = new App();
+// Prueba metodo factorial
+console.log("Factorial de 5 = "+app.factorial(5));
+console.log("Factorial de 6 = "+app.factorial(6));
+
