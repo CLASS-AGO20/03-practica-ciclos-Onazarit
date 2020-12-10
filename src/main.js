d8f3e97b1b4d2a2e134b0bfceb3c9414fdda509c
Onazarit <onazarit@ucol.mx>
2020-12-10 15:57:50 -0600

    Implemented function obtenerDivisibles

--- src/main.js
+++ src/main.js
@@ -15,6 +15,18 @@ export default class App {
         }
         return(string);
     }
+
+    obtenerDivisibles(numero){
+        let contador=0;
+        let i = numero;
+        do{
+            if(numero % i == 0){
+                contador++;
+            }
+            i--;
+        }while(i>=1)
+        return(contador);
+    }
 }
 
 let app = new App();
@@ -25,3 +37,7 @@ console.log("Factorial de 6 = "+app.factorial(6));
 // Prueba metodo convertirAString
 console.log("5 asteriscos: "+app.convertirAString(5));
 console.log("8 asteriscos: "+app.convertirAString(8));
+
+// Prueba metodo obtenerDivisibles
+console.log("El 6 es divisible entre "+app.obtenerDivisibles(6)+" numeros");
+console.log("El 10 es divisible entre "+app.obtenerDivisibles(10)+" numeros");
